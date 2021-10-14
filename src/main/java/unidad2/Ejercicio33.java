0b1311a4041c155434cc6151caf258dc45820805
jpabloprietog <alum.jpabloprietog.2021@iesalixar.org>
2021-10-14 17:15:03 +0200

    Ejercicios 2 y 3

---
+++ src/main/java/unidad2/Ejercicio33.java
@@ -0,0 +1,24 @@
+package unidad2;
+
+import java.util.Scanner;
+
+public class Ejercicio33 {
+
+    public static void main(String[] args) {
+
+        double numero1, numero2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un numero");
+        numero1 = scanner.nextDouble();
+
+        System.out.println("Introduce otro numero");
+        numero2 = scanner.nextDouble();
+
+        System.out.println(numero1/numero2);
+        System.out.println("El resultado es:" + (numero1/numero2));
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/unidad2/Ejercicio34.java
@@ -0,0 +1,20 @@
+package unidad2;
+
+import java.util.Scanner;
+
+public class Ejercicio34 {
+
+    public static void main(String[] args) {
+
+        float milla;
+        float kilometro = 1609;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el número de millas");
+        milla = scanner.nextFloat();
+        System.out.println(milla*kilometro);
+        System.out.println("la longitud es: " + (milla*kilometro) + " kilometros");
+
+        scanner.close();
+    }
+}
