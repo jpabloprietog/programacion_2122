79bc7b8515f48e43c02ac17f737224f963b24320
jpabloprietog <alum.jpabloprietog.2021@iesalixar.org>
2021-10-20 17:56:35 +0200

    Ejercicios nuevos

--- src/main/java/unidad4/Ejercicio42.java
+++ src/main/java/unidad4/Ejercicio42.java
@@ -1,4 +1,21 @@
 package unidad4;
 
+import java.util.Scanner;
+
 public class Ejercicio42 {
+
+    public static void main(String[] args) {
+
+        int numero = 12;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el número 12");
+        numero = scanner.nextInt();
+
+        if (numero == 12){
+            System.out.println("Es correcto");
+        } else {
+            System.out.println("No es correcto");
+        }
+    }
 }
